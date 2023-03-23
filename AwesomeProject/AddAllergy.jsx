701ec19d653faf4ff665ac772fcb316ee7f5d72f
Kate Dollete <cfdollete@up.edu.ph>
2023-03-23 18:11:24 +0000

    Refactor code for adding allergies

--- AwesomeProject/AddAllergy.jsx
+++ AwesomeProject/AddAllergy.jsx
@@ -7,38 +7,49 @@ import {
   Text,
 } from "react-native";
 
-export const AddAllergy = ({ addAllergy }) => {
-  const [newAllergy, setNewAllergy] = useState("Enter your allergies");
+export const AddAllergy = ({ setAllergies }) => {
+  const [newAllergy, setNewAllergy] = useState("");
 
-  const onChange = (textValue) => setNewAllergy(textValue);
+  const handleInput = (newAllergy) => {
+    setAllergies((prevAllergies) => {
+      return [...prevAllergies, newAllergy];
+    });
+    setNewAllergy("");
+  };
 
   return (
     <View>
       <TextInput
-        placeholder="Allergies"
+        placeholder={"Enter your allergies"}
         style={styles.input}
-        onChangeText={onChange}
-      ></TextInput>
-      <TouchableOpacity onPress={() => addAllergy(newAllergy)}>
-        <Text>Add Item</Text>
+        value={newAllergy}
+        onChangeText={(value) => setNewAllergy(value)}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => handleInput(newAllergy)}
+      >
+        <Text style={styles.btnText}>+ Add Item</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   input: {
     borderColor: "#000",
     borderWidth: 1,
     borderRadius: 5,
     marginVertical: 10,
     padding: 4,
-    width: 220,
+  },
+  btn: {
+    backgroundColor: "#c2bad8",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  btnText: {
+    textAlign: "center",
   },
 });
--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,41 +1,12 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { ListAllergies } from "./ListAllergies";
 
 export default function App() {
-  const [allergies, setAllergies] = useState([]);
-  const [newAllergy, setNewAllergy] = useState("");
-
-  const onChange = (textValue) => setNewAllergy(textValue);
-
   return (
     <View style={styles.container}>
-      <Text>Hello team Tech Capsule!</Text>
-      <TextInput
-        placeholder="Enter your allergies"
-        style={styles.input}
-        onPress={() => setNewAllergy("")}
-        onChangeText={onChange}
-      ></TextInput>
-      <TouchableOpacity
-        onPress={() => {
-          setAllergies((prevAllergies) => {
-            return [...prevAllergies, newAllergy];
-          });
-        }}
-      >
-        <Text>Add Item</Text>
-      </TouchableOpacity>
-      <FlatList
-        data={allergies}
-        renderItem={({ item }) => <Text>{item}</Text>}
-      />
+      <Text style={styles.title}>Hello team Tech Capsule!</Text>
+      <ListAllergies />
     </View>
   );
 }
@@ -43,16 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "stretch",
+    marginTop: 80,
+    marginHorizontal: 10,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  input: {
-    borderColor: "#000",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginVertical: 10,
-    padding: 4,
-    width: 220,
-  },
+  title: { textAlign: "center", marginBottom: 20 },
 });
