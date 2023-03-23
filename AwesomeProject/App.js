ed1ea53a93346e5eaf689cf6e4db23173a6e5674
Jason Chan <jason@Jasons-Air.home>
2023-03-23 21:46:46 +0000

    this commit will setup firebase

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbi_pa_vzAW7v0EiHkLUJwObA5a9jky0c",
+  authDomain: "capsulert-project.firebaseapp.com",
+  projectId: "capsulert-project",
+  storageBucket: "capsulert-project.appspot.com",
+  messagingSenderId: "892192871729",
+  appId: "1:892192871729:web:eab3cfc4447cbeeb7acc10",
+  measurementId: "G-MFQXF1ZV56",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
