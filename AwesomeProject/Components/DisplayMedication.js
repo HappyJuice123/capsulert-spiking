63de29859e1ecfc81d2919461a5fbe9443604118
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-24 21:16:11 +0000

    successfully rendered the name and description from the nhs api, tested with two different medications

--- AwesomeProject/Components/DisplayMedication.js
+++ AwesomeProject/Components/DisplayMedication.js
@@ -1,74 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { getMedication } from "../api";
-import { FlatList, Text, View } from "react-native";
+import { getMedication } from "../Utils/api";
+import { Text, View } from "react-native";
 
 export default function DisplayMedication() {
-  const [medication, setMedication] = useState([]);
+  const [name, setName] = useState("");
+  const [descriptions, setDescriptions] = useState([]);
 
   const medicationName = "Rivaroxaban";
-
-  //   Gliclazide
-  //   Rivaroxaban
+  // Test Medication name: Rivaroxaban, Gliclazide
 
   useEffect(() => {
-    getMedication(medicationName).then((medicationData) => {
-      setMedication(medicationData);
+    getMedication(medicationName).then((responseData) => {
+      console.log(responseData, "<<< response data object");
+      console.log(responseData.name, "<<<< Medication Name");
+      console.log(
+        responseData.hasPart,
+        "<<< Array of nested objects with medication descriptions"
+      );
+      setName(responseData.name);
+      setDescriptions(responseData.hasPart);
     });
   }, []);
 
-  console.log(medication);
-
   return (
     <View>
-      <Text>{medication.name}</Text>
-      {/* <FlatList
-        style={styles.list}
-        data={medication.mainEntityOfPage}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.listItem}>
-            <View>
-              <Text>{item}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      ></FlatList> */}
+      <Text>{name}</Text>
+      <Text>
+        {/* FlatList to display each item description */}
+        {descriptions.map((item) => {
+          return item.description;
+        })}
+      </Text>
     </View>
   );
-
-  //   const Item = ({ title, description, index }) => (
-  //     <View>
-  //       <Text style={styles.title}>
-  //         {index}. {title}
-  //       </Text>
-  //       <Text> {description} </Text>
-  //     </View>
-  //   );
-
-  //   const renderItem = ({ item, index }) => (
-  //     <Item description={item.description} title={item.title} index={index} />
-  //   );
-
-  //   return (
-  //     <View>
-  //       {<FlatList data={data} renderItem={renderItem} keyExtractor={index}/>}
-  //     </View>
-  //   );
 }
-
-// const styles = StyleSheet.create({
-//   listItem: {
-//     display: "flex",
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     gap: 50,
-//     padding: 15,
-//     backgroundColor: "#f8f8f8",
-//     borderBottomWidth: 1,
-//     borderColor: "#eee",
-//   },
-//   checkbox: {
-//     display: "flex",
-//     flexDirection: "row",
-//     gap: 10,
-//   },
-// });
