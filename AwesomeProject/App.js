0eb2e992d47059daba963d6a5e41d82348cc23be
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-24 09:44:39 +0000

    initial branch commit

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello team Tech Capsule!</Text>
+      <Text>Spiking for NHS API Data Visualition</Text>
       <StatusBar style="auto" />
     </View>
   );
