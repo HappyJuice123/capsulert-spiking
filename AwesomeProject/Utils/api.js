9f8f8b06b17ce680861500228ca4903f91ee024c
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-24 21:14:31 +0000

    successful get request using axios to nhs api

---
+++ AwesomeProject/Utils/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseUrl = "https://api.nhs.uk/medicines";
+
+export const getMedication = (medicationName) =>
+  axios({
+    method: "get",
+    headers: {
+      "subscription-key": "8b1256c6a267411eab1bc7b1bb78833b",
+    },
+    url: `${baseUrl}/${medicationName}`,
+  }).then((response) => {
+    return response.data;
+  });
