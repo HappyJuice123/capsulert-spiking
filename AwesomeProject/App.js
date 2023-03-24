1af078bdedc8715d5383c8e1a9f8a4df1da3b9c3
Jason Chan <jason@Jasons-Air.home>
2023-03-24 13:56:46 +0000

    this commit will attempt in using clarifai

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { initializeApp } from "firebase/app";
+import React from "react";
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbi_pa_vzAW7v0EiHkLUJwObA5a9jky0c",
@@ -14,11 +17,33 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const Clarifai = require("clarifai");
+
+const clarifai = new Clarifai.App({
+  apiKey: "361302f22e754af7ae32fa1efcdb9a0d",
+});
+
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Hello team Tech Capsule!</Text>
       <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -31,3 +56,96 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const USER_ID = "jason1201";
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = "bee2af2ea9864622b5b1eea2faaaf6a4";
+const APP_ID = "my-first-application";
+// Change these to build your own VTR Workflow
+const WORKFLOW_ID = "visual-text-recognition-id";
+
+const WORKFLOWNODE_ID_1 = "detect-concept";
+const MODEL_ID_1 = "2419e2eae04d04f820e5cf3aba42d0c7";
+const MODEL_VERSION_ID_1 = "75a5b92a0dec436a891b5ad224ac9170";
+
+const WORKFLOWNODE_ID_2 = "image-crop";
+const MODEL_ID_2 = "ce3f5832af7a4e56ae310d696cbbefd8";
+const MODEL_VERSION_ID_2 = "a78efb13f7774433aa2fd4864f41f0e6";
+
+const WORKFLOWNODE_ID_3 = "image-to-text";
+const MODEL_ID_3 = "9fe78b4150a52794f86f237770141b33";
+const MODEL_VERSION_ID_3 = "d94413e582f341f68884cac72dbd2c7b";
+
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+// This will be used by every Clarifai endpoint call
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+stub.PostWorkflows(
+  {
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    workflows: [
+      {
+        id: WORKFLOW_ID,
+        nodes: [
+          {
+            id: WORKFLOWNODE_ID_1,
+            model: {
+              id: MODEL_ID_1,
+              model_version: {
+                id: MODEL_VERSION_ID_1,
+              },
+            },
+          },
+          {
+            id: WORKFLOWNODE_ID_2,
+            model: {
+              id: MODEL_ID_2,
+              model_version: {
+                id: MODEL_VERSION_ID_2,
+              },
+            },
+            node_inputs: [
+              {
+                node_id: WORKFLOWNODE_ID_1,
+              },
+            ],
+          },
+          {
+            id: WORKFLOWNODE_ID_3,
+            model: {
+              id: MODEL_ID_3,
+              model_version: {
+                id: MODEL_VERSION_ID_3,
+              },
+            },
+            node_inputs: [
+              {
+                node_id: WORKFLOWNODE_ID_2,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  metadata,
+  (err, response) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    if (response.status.code !== 10000) {
+      console.log(response.status);
+      throw new Error(
+        "Post workflows failed, status: " + response.status.description
+      );
+    }
+  }
+);
