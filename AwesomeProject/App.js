860e502de7a4a67e56b852c202c59d5af7dc299c
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-25 21:42:05 +0000

    successful import of tailwindcss/nativewind, example renders text in four different colours

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,33 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { initializeApp } from "firebase/app";
+import { Text, View } from "react-native";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbi_pa_vzAW7v0EiHkLUJwObA5a9jky0c",
-  authDomain: "capsulert-project.firebaseapp.com",
-  projectId: "capsulert-project",
-  storageBucket: "capsulert-project.appspot.com",
-  messagingSenderId: "892192871729",
-  appId: "1:892192871729:web:eab3cfc4447cbeeb7acc10",
-  measurementId: "G-MFQXF1ZV56",
-};
+// import { initializeApp } from "firebase/app";
 
-const app = initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBbi_pa_vzAW7v0EiHkLUJwObA5a9jky0c",
+//   authDomain: "capsulert-project.firebaseapp.com",
+//   projectId: "capsulert-project",
+//   storageBucket: "capsulert-project.appspot.com",
+//   messagingSenderId: "892192871729",
+//   appId: "1:892192871729:web:eab3cfc4447cbeeb7acc10",
+//   measurementId: "G-MFQXF1ZV56",
+// };
+
+// const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello team Tech Capsule!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text className="text-purple-800">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-red-800">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-yellow-800">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-green-800">
+        Open up App.js to start working on your app!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
