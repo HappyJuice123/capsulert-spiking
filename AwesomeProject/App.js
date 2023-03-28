6249cb8cd7bb598d0c002ba7bff9de1d051a179b
Kate Dollete <cfdollete@up.edu.ph>
2023-03-28 21:39:42 +0100

    Enable local notifications

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,24 +1,104 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { initializeApp } from "firebase/app";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import * as Notifications from "expo-notifications";
+import Device from "expo-device";
+import storage from "@react-native-async-storage/async-storage";
+import { useEffect, useRef, useState } from "react";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbi_pa_vzAW7v0EiHkLUJwObA5a9jky0c",
-  authDomain: "capsulert-project.firebaseapp.com",
-  projectId: "capsulert-project",
-  storageBucket: "capsulert-project.appspot.com",
-  messagingSenderId: "892192871729",
-  appId: "1:892192871729:web:eab3cfc4447cbeeb7acc10",
-  measurementId: "G-MFQXF1ZV56",
-};
-
-const app = initializeApp(firebaseConfig);
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    const getPermission = async () => {
+      if (Device.isDevice) {
+        const { status: existingStatus } =
+          await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+          alert("Enable push notifications to use the app!");
+          await storage.setItem("expopushtoken", "");
+          return;
+        }
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        await storage.setItem("expopushtoken", token);
+      } else {
+        alert("Must use physical device for Push Notifications");
+      }
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+    };
+
+    getPermission();
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  const onClick = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Title",
+        body: "body",
+        data: { data: "data goes here" },
+      },
+      trigger: {
+        hour: 20,
+        minute: 10,
+        repeats: true,
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Hello team Tech Capsule!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={onClick}>
+        <Text
+          style={{
+            backgroundColor: "red",
+            padding: 10,
+            color: "white",
+            marginTop: 10,
+          }}
+        >
+          Click me to send a push notification!
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
